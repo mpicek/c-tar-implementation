e2db2afdc29eade90e905163f7532293e570a924
mpicek <martinpicek4@gmail.com>
2020-07-12 13:10:31 +0200

    clang-format of mytar.c

--- mytar.c
+++ mytar.c
@@ -49,8 +49,8 @@ void unexp_EOF_err() {
 
 void more_than_one_action_argument_err() {
   errx(2, "You may not specify more than one \'-Acdtrux\', \'--delete\' or "
-  "\'--test-label\' option\nTry \'tar --help\' or \'tar --usage\' "
-  "for more information.");
+          "\'--test-label\' option\nTry \'tar --help\' or \'tar --usage\' "
+          "for more information.");
 }
 
 void ftell_unsuccessful() { errx(2, "Internal error - ftell unsuccessful"); }
@@ -60,10 +60,10 @@ void fseek_unsuccessful() { errx(2, "Internal error - fseek unsuccessful"); }
 void fputc_unsuccessful() { errx(2, "Internal error - fputc unsuccessful"); }
 
 void fopen_unsuccessful(char fileName[]) {
-    errx(2,
-         "%s: Cannot open: No such file or directory\n Error is not "
-         "recoverable: exiting now",
-         fileName);
+  errx(2,
+       "%s: Cannot open: No such file or directory\n Error is not "
+       "recoverable: exiting now",
+       fileName);
 }
 
 void magic_err() {
@@ -74,23 +74,22 @@ void magic_err() {
 
 void fclose_unsuccessful() { errx(2, "Internal error - fclose unsuccessful"); }
 
-struct action_info{
+struct action_info {
   char fileName[PATH_MAX]; // name of tar file
-  bool file;    // if user specified tar file
-  bool action_defined; // if action is even defined
-  bool list;    // if -t is defined
-  bool extract; // if -x is defined
-  bool verbose; // if -v is defined
+  bool file;               // if user specified tar file
+  bool action_defined;     // if action is even defined
+  bool list;               // if -t is defined
+  bool extract;            // if -x is defined
+  bool verbose;            // if -v is defined
 };
 
-void init_action_info(struct action_info *action){
+void init_action_info(struct action_info *action) {
   action->file = false;
   action->list = false;
   action->extract = false;
   action->verbose = false;
   action->action_defined = false;
 }
-
 
 void check_processed_files(struct files_to_process ftprocess) {
   if (ftprocess.defined) {
@@ -111,7 +110,7 @@ void init_files_to_process(struct files_to_process *ftprocess, int argc) {
   ftprocess->filenames =
       malloc(argc * sizeof(char *)); // allocate memory for an array of pointers
                                      // pointing at filenames
-  if (ftprocess->filenames == NULL) {  // check success of malloc()
+  if (ftprocess->filenames == NULL) { // check success of malloc()
     malloc_unsuccessful();
   }
   ftprocess->number = 0;
@@ -132,10 +131,12 @@ long long oct_to_dec(char *str) {
   while (str[i])
     result = 8 * (result + str[i++] - '0');
 
-  return result / 8; }
+  return result / 8;
+}
 
 /// safe seeking in file
-void safe_seek(FILE *f, long long offset, int relative_point,long long file_length) {
+void safe_seek(FILE *f, long long offset, int relative_point,
+               long long file_length) {
   if (relative_point == SEEK_CUR) {
     long long current_offset = ftell(f);
     if (current_offset == -1) {
@@ -153,7 +154,8 @@ void safe_seek(FILE *f, long long offset, int relative_point,long long file_leng
 }
 
 /// reads string from file and returns to the original position
-void read_string(FILE *f, long long location, char str[], long long str_length, long long file_length) {
+void read_string(FILE *f, long long location, char str[], long long str_length,
+                 long long file_length) {
 
   long long positionThen = ftell(f); // position at the beginning of the process
   if (positionThen == -1) {
@@ -175,34 +177,35 @@ void read_string(FILE *f, long long location, char str[], long long str_length, 
 }
 
 // saves name of the current file to name[]
-void get_name_of_file(FILE *f, char name[], long long file_length){
+void get_name_of_file(FILE *f, char name[], long long file_length) {
   read_string(f, NAME_LOCATION, name, NAME_LENGTH, file_length); // read NAME
 }
 
-void get_flag_of_file(FILE *f, char typeflag[], long long file_length){
-  read_string(f, TYPEFLAG_LOCATION, typeflag, 
-        TYPEFLAG_LENGTH, file_length); // read TYPEFLAG
-} 
+void get_flag_of_file(FILE *f, char typeflag[], long long file_length) {
+  read_string(f, TYPEFLAG_LOCATION, typeflag, TYPEFLAG_LENGTH,
+              file_length); // read TYPEFLAG
+}
 
-void get_size_of_file(FILE *f, char sizeStr[], long long file_length){
+void get_size_of_file(FILE *f, char sizeStr[], long long file_length) {
   read_string(f, SIZE_LOCATION, sizeStr, SIZE_LENGTH, file_length); // read SIZE
 }
 
-void check_magic(FILE *f, long long file_length){
+void check_magic(FILE *f, long long file_length) {
   char magic[6];
   read_string(f, MAGIC_LOCATION, magic, MAGIC_LENGTH, file_length);
-  if(strcmp(magic, PROPER_MAGIC) != 0){
+  if (strcmp(magic, PROPER_MAGIC) != 0) {
     magic_err();
   }
 }
 
-void print_file(char name[], struct files_to_process *ftprocess, bool delete_from_files_to_process) {
+void print_file(char name[], struct files_to_process *ftprocess,
+                bool delete_from_files_to_process) {
   if (ftprocess->defined) {
     for (int i = 0; i < ftprocess->number; ++i) {
       if (strcmp(ftprocess->filenames[i], name) == 0) {
         printf("%s\n", name);
         fflush(stdout);
-        if(delete_from_files_to_process)
+        if (delete_from_files_to_process)
           ftprocess->filenames[i][0] = '\0'; // it means the file is printed
         break;
       }
@@ -228,18 +231,18 @@ long long get_tar_file_length(FILE *f) {
   return file_length;
 }
 
-void create_and_extract_to_file(FILE *f, char name[], long long size, 
-              long long file_length, struct files_to_process ftprocess){
+void create_and_extract_to_file(FILE *f, char name[], long long size,
+                                long long file_length,
+                                struct files_to_process ftprocess) {
 
   long long positionThen = ftell(f); // position at the beginning of the process
   if (positionThen == -1) {
     ftell_unsuccessful();
   }
 
-
   bool process_file = false;
 
-  if(ftprocess.defined){
+  if (ftprocess.defined) {
     for (int i = 0; i < ftprocess.number; ++i) {
       if (!strcmp(ftprocess.filenames[i], name)) {
         process_file = true;
@@ -247,27 +250,26 @@ void create_and_extract_to_file(FILE *f, char name[], long long size,
         break;
       }
     }
-  }
-  else{
+  } else {
     process_file = true;
   }
 
-  if(process_file){
-    //create new file
+  if (process_file) {
+    // create new file
     FILE *extracted_file;
     extracted_file = fopen(name, "w");
-    if(extracted_file == NULL)
+    if (extracted_file == NULL)
       fopen_unsuccessful(name);
 
     safe_seek(f, MULTIPLE, SEEK_CUR, file_length); // skip header
 
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
       int one_byte;
       one_byte = fgetc(f);
-      if(one_byte == EOF)
+      if (one_byte == EOF)
         unexp_EOF_err();
       int is_error = fputc((char)one_byte, extracted_file);
-      if(is_error == EOF)
+      if (is_error == EOF)
         fputc_unsuccessful();
     }
 
@@ -280,12 +282,14 @@ void create_and_extract_to_file(FILE *f, char name[], long long size,
       ftell_unsuccessful();
     }
     safe_seek(f, (positionThen - positionNow), SEEK_CUR,
-            file_length); // seek back
+              file_length); // seek back
   }
 }
 
 /// returns 1 when reads file, 0 when there is no file
-bool process_one_file_and_jump(FILE *f, struct action_info action, struct files_to_process ftprocess, long long file_length){
+bool process_one_file_and_jump(FILE *f, struct action_info action,
+                               struct files_to_process ftprocess,
+                               long long file_length) {
 
   char name[NAME_LENGTH];
   char sizeStr[SIZE_LENGTH];
@@ -297,26 +301,27 @@ bool process_one_file_and_jump(FILE *f, struct action_info action, struct files_
   if (name[0] == 0)
     return false; // when this is an empty block at the end of the .tar file
 
-  //read_string(f, SIZE_LOCATION, sizeStr, SIZE_LENGTH, file_length); // read SIZE
+  // read_string(f, SIZE_LOCATION, sizeStr, SIZE_LENGTH, file_length); // read
+  // SIZE
   get_size_of_file(f, sizeStr, file_length);
   long long size = oct_to_dec(sizeStr);
 
-  if(action.list){
-  // when it is not regular file but there is file (name[0] != 0)
+  if (action.list) {
+    // when it is not regular file but there is file (name[0] != 0)
     if ((typeflag[0] - '0' != REGULAR_FILE_FLAG) && (name[0] != 0)) {
       errx(2, "Unsupported header type: %d", typeflag[0]);
     }
     print_file(name, &ftprocess, true);
-  }
-  else if(action.extract){
+  } else if (action.extract) {
     // when it is not regular file but there is file (name[0] != 0)
-    if ((typeflag[0] - '0' != REGULAR_FILE_FLAG && 
-         typeflag[0] - '0' != DIRECTORY_FILE_FLAG) && (name[0] != 0)) {
+    if ((typeflag[0] - '0' != REGULAR_FILE_FLAG &&
+         typeflag[0] - '0' != DIRECTORY_FILE_FLAG) &&
+        (name[0] != 0)) {
       errx(2, "Unsupported header type: %d", typeflag[0]);
     }
-    if(action.verbose)
+    if (action.verbose)
       print_file(name, &ftprocess, false);
-    if(strcmp(name, "./") != 0)
+    if (strcmp(name, "./") != 0)
       create_and_extract_to_file(f, name, size, file_length, ftprocess);
   }
 
@@ -334,21 +339,22 @@ bool process_one_file_and_jump(FILE *f, struct action_info action, struct files_
   return true;
 }
 
-void process_action(struct action_info action, struct files_to_process ftprocess){
+void process_action(struct action_info action,
+                    struct files_to_process ftprocess) {
   if (action.fileName[0] == 0)
     errx(2, "tar: Refusing to read archive contents from terminal (missing -f "
             "option?)\ntar: Error is not recoverable: exiting now");
   FILE *f = fopen(action.fileName, "r");
   if (f == NULL)
     fopen_unsuccessful(action.fileName);
-  
+
   long long file_length = get_tar_file_length(f);
 
   check_magic(f, file_length);
 
   bool fileRead = true;
   while (fileRead) {
-    //PROCESS FILE
+    // PROCESS FILE
     fileRead = process_one_file_and_jump(f, action, ftprocess, file_length);
     long long current_offset = ftell(f);
 
@@ -378,23 +384,24 @@ void process_action(struct action_info action, struct files_to_process ftprocess
   if (fclose(f) == EOF) {
     fclose_unsuccessful();
   }
-
 }
 
-void process_tar_archive(struct action_info action, struct files_to_process ftprocess){
-  if(action.list && action.extract){
+void process_tar_archive(struct action_info action,
+                         struct files_to_process ftprocess) {
+  if (action.list && action.extract) {
     more_than_one_action_argument_err();
-  } else{
-    if(action.action_defined){
-      if(action.file){
-        //TODO action
+  } else {
+    if (action.action_defined) {
+      if (action.file) {
+        // TODO action
         process_action(action, ftprocess);
         fflush(stdout);
-      } else{
-        errx(2, "tar: Refusing to read archive contents from terminal (missing -f "
-                "option?)\ntar: Error is not recoverable: exiting now");
+      } else {
+        errx(2,
+             "tar: Refusing to read archive contents from terminal (missing -f "
+             "option?)\ntar: Error is not recoverable: exiting now");
       }
-    } else{
+    } else {
       errx(2, "tar: You must specify one of the '-Acdtrux', '--delete' or "
               "'--test-label' options\nTry 'tar --help' or 'tar --usage' for "
               "more information.");
@@ -406,7 +413,8 @@ void strcpy_unsuccessful() {
   errx(2, "Internal error - can't use strcpy - destination is too small");
 }
 
-void safe_strcpy(char *dest, long long dest_len, char *source, long long source_len) {
+void safe_strcpy(char *dest, long long dest_len, char *source,
+                 long long source_len) {
   if (dest_len >= source_len)
     strcpy(dest, source);
   else
@@ -430,25 +438,26 @@ void program(int argc, char *argv[]) {
     if (!strcmp(argv[i], "-f")) { // FILENAME
       action.file = true;
       i++; // next argument should be fileName
-      if (i == argc){
+      if (i == argc) {
         errx(2, "tar: option requires an argument -- 'f'\nTry 'tar --help' or "
                 "'tar --usage' for more information.");
       }
 
-      safe_strcpy(action.fileName, sizeof(action.fileName), argv[i], sizeof(argv[i]));
+      safe_strcpy(action.fileName, sizeof(action.fileName), argv[i],
+                  sizeof(argv[i]));
 
     } else if (!strcmp(argv[i], "-t")) { // LIST
       action.list = true;
       action.action_defined = true;
-    } else if (!strcmp(argv[i], "-x")){ // EXTRACT
+    } else if (!strcmp(argv[i], "-x")) { // EXTRACT
       action.extract = true;
       action.action_defined = true;
-    } else if (!strcmp(argv[i], "-v")){ // VERBOSE
+    } else if (!strcmp(argv[i], "-v")) { // VERBOSE
       action.verbose = true;
       action.action_defined = true;
-    } else {      // OTHER OPTION OR FILENAME
+    } else {                               // OTHER OPTION OR FILENAME
       if (action.list || action.extract) { // if list or extract defined
-                  //than the next option is the file to be listed/extracted
+        // than the next option is the file to be listed/extracted
         ftprocess.defined = 1;
         ftprocess.filenames[ftprocess.number] =
             malloc(PATH_MAX * sizeof(char)); // allocate memory
@@ -459,14 +468,14 @@ void program(int argc, char *argv[]) {
                     sizeof(ftprocess.filenames[ftprocess.number]), argv[i],
                     sizeof(argv[i]));
         ftprocess.number++;
-      } else{
+      } else {
         // commented to pass the tests:
         // errx(2, "Unknown option.");
         exit(2); // UNKNOWN OPTION
-        }
+      }
     }
   }
-  
+
   process_tar_archive(action, ftprocess);
 
   deallocate_files_to_process(&ftprocess);
